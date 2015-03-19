a36d1524ad8d294c562bd817c99f4948baa214bc
Raymond Cox <raymond@helios.raymondjcox.com>
2015-03-18 20:59:41 -0400

    Remove deprecated ArrayController

--- app/controllers/posts/post.js
+++ app/controllers/posts/post.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   hasComment: Ember.computed.gt('numComments', 0),
   numComments: Ember.computed.readOnly('kids.length')
 });
